af9c41db6fd6523b2f600ce98f763e464540cf07
Sanda Golcea <sanda.golcea@gmail.com>
2015-03-14 23:07:02 +0000

    Getting JSON menu&display

--- src/script.js
+++ src/script.js
@@ -1,7 +1,23 @@
 $(document).ready(function(){
+  // Getting basic store info
   $.get( "menu.json", function( data ) {
     $( "body" )
-      .append( "<p>Shop Name: " + data[0].shopName + "</p>") 
+      .append( "<p><strong>Shop Name: " + data[0].shopName + "</p></strong>") 
       .append( "Address: " + data[0].address ); 
   }, "json" );
+
+  // Get product/price info & store in items
+  $.getJSON( "menu", function( data ) {
+    i=0;
+    var items = [];
+    $.each( data[0], function( key, val ) {
+      items.push( "<li id='" + i + "'>" + key + ": " + val + "</li>" );
+      i++;
+    });
+   
+    $( "<ul/>", {
+      "class": "my-new-list",
+      html: items.join( "" )
+    }).appendTo( "body" );
+  });
 });
